a06fec7bf690a4df3cb43118267f9388e093e950
Jada Smith <jada.smith@lethpolytech.ca>
2024-10-09 09:37:08 -0600

    psuedo code

---
+++ main.cpp
@@ -0,0 +1,33 @@
+/*
+open the source
+open the target 
+try to get a char from the source
+while not at the end of the source
+	if the source char is "#"
+		get another char
+			if that char is "N"
+				get another char
+			if that char is "#" 
+				write the selected name
+			else 
+				write "#N" and the two other chars
+		else 
+			write "#" and write that char
+	else
+		write chars from the source to target
+close source
+close target
+*/
+
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
+int main() {
+
+
+
+	return 0;
+}
